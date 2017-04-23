f832ffa462f3bb6badd343fac0ac3ab043543486
Ben Bales <bbbales2@gmail.com>
2017-04-22 17:34:17 -0700

    Updated test run script

--- test/generate_tests.py
+++ test/generate_tests.py
@@ -18,6 +18,7 @@ from rotations import inv_rotations
 
 Ps = [(10, 286), (12, 455)]
 Ns = range(20, 31)
+J = 4
 
 def func(P, N, c11, c12, c44, density, w, x, y, z, X, Y, Z):
     #$c12 = -(c44 * 2.0 / anisotropic - c11)
@@ -40,16 +41,15 @@ def func(P, N, c11, c12, c44, density, w, x, y, z, X, Y, Z):
 
 # Data from https://arxiv.org/pdf/1605.09237.pdf and http://www.coolmagnetman.com/magconda.htm and https://en.wikipedia.org/wiki/Nickel
 cs = [[1.684, 1.214, 0.754, 8960.0], [1.073, 0.6008, 0.283, 2700.0], [2.53, 1.52, 1.24, 8908.0]]
-qsub = """
-#!/bin/bash
+qsub = """#!/bin/bash
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=48:00:00
+#PBS -l walltime=24:00:00
 
 export OMP_NUM_THREADS=1
 
 cd $PBS_O_WORKDIR
 
-/home/bbales2/modal_cpp/models/cubic_w_rotations sample algorithm=hmc engine=nuts max_depth=7 num_samples=1000 num_warmup=1000 save_warmup=1 data file=input.dat output file=$PBS_JOBID.csv refresh=1
+/home/bbales2/modal_cpp/models/cubic_w_rotations sample algorithm=hmc engine=nuts max_depth=7 num_samples=1000 num_warmup=1000 save_warmup=1 id=$(echo $PBS_JOBID | sed -e 's/[^0-9]//g') data file=input.dat output file=$PBS_JOBID.csv refresh=1
 """
 
 tmp = """
@@ -63,7 +63,7 @@ L <- {6}
 y <- c({7})
 """
 
-for i in range(1):
+for i in range(50):
     sigma = numpy.random.uniform(0.1, 0.4)
 
     X = numpy.random.uniform(0.005, 0.02)
@@ -102,3 +102,8 @@ for i in range(1):
 
     with open("{0}/run.qsub".format(i), "w") as f:
         f.write(qsub)
+
+    print "cd {0}".format(i)
+    for j in range(J):
+        print "qsub run.qsub".format(i)
+    print "cd -"
